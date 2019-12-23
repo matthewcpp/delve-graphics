6ee48dd460377e98f93851d9455fc0979409837d
Matthew LaRocca <mlarocca.cpp@gmail.com>
2019-12-23 05:53:07 -0800

    generate script to compile shaders into build directory

--- cmake/FindVulkan.cmake
+++ cmake/FindVulkan.cmake
@@ -2,15 +2,20 @@ if (WIN32)
     set(vk_include_path Include)
     set(vk_library_name vulkan-1.lib)
     set(vk_library_path Lib)
+    set(vk_glsl_compiler glslc.exe)
+    set(vk_bin_dir Bin)
 
     if("${CMAKE_SIZEOF_VOID_P}" STREQUAL "4")
         set(vk_library_path ${vk_library_path}32)
+        set(vk_bin_dir ${vk_bin_dir}32)
     endif()
     
 elseif(MACOS)
     set(vk_include_path MoltenVK/include)
     set(vk_library_name MoltenVK)
     set(vk_library_path MoltenVK/macOS/dynamic)
+    set(vk_glsl_compiler glslc)
+    set(vk_bin_dir macOS/bin)
 elseif (UNIX)
     set(vk_include_path x86_64/include)
     set(vk_library_name vulkan)
@@ -18,14 +23,19 @@ elseif (UNIX)
 endif()
 
 find_path(Vulkan_INCLUDE_DIR
-        NAMES vulkan/vulkan.h
-        HINTS ${VULKAN_SDK}
-        PATH_SUFFIXES ${vk_include_path})
+    NAMES vulkan/vulkan.h
+    HINTS ${VULKAN_SDK}
+    PATH_SUFFIXES ${vk_include_path})
 
 find_library(Vulkan_LIBRARY
-        NAMES ${vk_library_name}
-        HINTS ${VULKAN_SDK}
-        PATH_SUFFIXES ${vk_library_path})
+    NAMES ${vk_library_name}
+    HINTS ${VULKAN_SDK}
+    PATH_SUFFIXES ${vk_library_path})
+
+find_file(Vulkan_GLSL_COMPILER
+    NAMES ${vk_glsl_compiler}
+    HINTS ${VULKAN_SDK}
+    PATH_SUFFIXES ${vk_bin_dir})
 
 set(Vulkan_LIBRARIES ${Vulkan_LIBRARY})
 set(Vulkan_INCLUDE_DIRS ${Vulkan_INCLUDE_DIR})
@@ -34,7 +44,7 @@ include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(Vulkan
     DEFAULT_MSG
-    Vulkan_LIBRARY Vulkan_INCLUDE_DIR)
+    Vulkan_LIBRARY Vulkan_INCLUDE_DIR Vulkan_GLSL_COMPILER)
 
 mark_as_advanced(Vulkan_INCLUDE_DIR Vulkan_LIBRARY)
 
---
+++ scripts/compile_shaders.py
@@ -0,0 +1,42 @@
+import sys
+import os
+import subprocess
+
+def main():
+    if len(sys.argv) < 4:
+        print("usage: <glsl_compiler_path> <src_shader_dir> <dst_shader_dir>")
+        return 1
+
+    glsl_compiler = sys.argv[1]
+    src_dir = sys.argv[2]
+    dst_dir = sys.argv[3]
+
+    if not os.path.isfile(glsl_compiler):
+        print("invalid glsl compiler path: {}".format(glsl_compiler))
+        return 1
+
+    if not os.path.isdir(src_dir):
+        print("invalid source directory path: {}".format(glsl_compiler))
+        return 1
+
+    if not os.path.isdir(dst_dir):
+        print("creating destination dir: {}".format(dst_dir))
+        os.makedirs(dst_dir)
+
+    for item in os.listdir(src_dir):
+        src_path = os.path.join(src_dir, item)
+
+        if os.path.isfile(src_path):
+            dst_path = os.path.join(dst_dir, "{}.spv".format(item))
+            print("{} --> {}".format(src_path, dst_path))
+            try:
+                args = [glsl_compiler, src_path, "-o", dst_path]
+                result = subprocess.check_output(args)
+                if len(result) > 0:
+                    print(result)
+            except subprocess.CalledProcessError as compile_error:
+                print(compile_error.output)
+                return 1
+
+if __name__ == '__main__':
+    sys.exit(main())
